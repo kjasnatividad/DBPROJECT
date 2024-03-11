cdd1ef336a5dff1d0fc7fe0d41707907f1071eb9
kjasnatividad <kjasnatividad@addu.edu.ph>
2024-03-11 13:50:16 +0800

    Added frmUser

--- frmMain.cs
+++ frmMain.cs
@@ -83,6 +83,21 @@ namespace DBPROJECT
         {
             UserProfilefrm.Dispose();
         }
+
+        frmUser Userfrm;
+
+        private void usersToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Userfrm = new frmUser();
+            Userfrm.FormClosed += Userfrm_FormClosed;
+            Userfrm.MdiParent = this;
+            Userfrm.Show();
+        }
+
+        private void Userfrm_FormClosed(object sender, EventArgs e)
+        {
+            Userfrm.Dispose();
+        }
     }
 }
     
---
+++ frmUser.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBPROJECT
+{
+    public partial class frmUser : Form
+    {
+        DataTable DTable;
+
+        SqlDataAdapter DAdapter;
+        SqlCommand Dcommand;
+        BindingSource DBindingSource;
+
+        int idcolumn = 0;
+        public frmUser()
+        {
+            InitializeComponent();
+        }
+
+        private void frmUser_Load(object sender, EventArgs e)
+        {
+            this.BindMainGrid();
+            this.FormatGrid();
+        }
+
+        private void BindMainGrid() 
+        {
+            if(Globals.glOpenSqlConn())
+            {
+                this.Dcommand = new SqlCommand("spGetAllUsers", Globals.sqlconn);
+                this.DAdapter = new SqlDataAdapter(this.Dcommand); //link between table and sqlcommand
+
+                this.DTable = new DataTable();
+
+                this.DAdapter.Fill(DTable);
+
+                this.DBindingSource = new BindingSource(); //link between a navigator and a data table
+                this.DBindingSource.DataSource = DTable;
+
+                dgvMain.DataSource = DTable;
+
+                this.bNavMain.BindingSource = this.DBindingSource;
+            }
+        }
+
+        private void FormatGrid() 
+        {
+            this.dgvMain.Columns["id"].Visible = false;
+
+            this.dgvMain.Columns["loginname"].HeaderText = "Login Name";
+            this.dgvMain.Columns["active"].HeaderText = "Active";
+            this.dgvMain.Columns["mustchangepwd"].HeaderText = "Must Change PWD";
+            this.dgvMain.Columns["email"].HeaderText = "Email";
+            this.dgvMain.Columns["smtphost"].HeaderText = "SMTP Host";
+            this.dgvMain.Columns["smtpport"].HeaderText = "SMTP Port";
+            this.dgvMain.Columns["gender"].HeaderText = "Gender";
+            this.dgvMain.Columns["birthdate"].HeaderText = "Birthdate";
+
+            this.BackColor = Globals.gDialogBackgroundColor;
+
+            this.dgvMain.BackgroundColor = Globals.gGridOddRowColor;
+            this.dgvMain.AlternatingRowsDefaultCellStyle.BackColor = Globals.gGridEvenRowColor;
+
+            this.dgvMain.EnableHeadersVisualStyles = false;
+            this.dgvMain.ColumnHeadersDefaultCellStyle.BackColor = Globals.gGridHeaderColor;
+        }
+    }
+}
